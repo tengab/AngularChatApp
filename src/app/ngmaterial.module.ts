baf62854ea912e890856c97e9c80db42201da883
tengab <tomaszbanach01@gmail.com>
2018-04-05 22:48:59 +0200

    simple notification card ready

--- src/app/ngmaterial.module.ts
+++ src/app/ngmaterial.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {  MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule]
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule]
 })
 export class MaterialAppModule { }
